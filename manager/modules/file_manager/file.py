a95491bc5b823b6d3996cda06d6aa840201b4fc5
Guilherme Rodrigues <guilherme.b.rodrigues@skf.com>
2022-03-17 12:29:46 -0300

    first version

---
+++ manager/modules/file_manager/file.py
@@ -0,0 +1,53 @@
+import logging
+import shutil
+from operator import contains
+from modules.file_manager.util import is_file
+
+class File:
+
+    def __init__(self) -> None:
+        self.logger = logging.getLogger(__name__)
+
+    def copy_single(self, source_file: str, dest_file: str) -> str:
+        
+        if not is_file(source_file):
+            raise Exception(f"{source_file} is not a file")
+
+        try:
+            dest = shutil.copyfile(source_file, dest_file)
+            self.logger.debug(f"file was copied from {source_file} to {dest_file}")
+
+            return dest
+        except Exception as err:
+            self.logger.exception(f"unable to copy file from {source_file} to {dest_file}")
+            raise err
+
+    def update_content(self, file_path:str, contains_in_line:str, replace_by:str) -> None:
+        
+        
+        contains_in_line = contains_in_line.lower()
+
+        try:
+            text_line = None
+
+            with open(file_path, 'r+', encoding='latin-1') as file:
+                lines = file.readlines()
+
+                for line in lines:
+                    if contains(line.lower(), contains_in_line):
+                        text_line = line
+                        break
+
+                if text_line is None:
+                    return
+
+                file.seek(0)
+                content = file.read()
+                content = content.replace(text_line.strip(), replace_by)
+                file.truncate(0)
+                file.seek(0)
+                file.write(content)
+        except Exception as err:
+            self.logger.exception("unable to update the content")
+            raise err
+    
---
+++ manager/modules/file_manager/folder.py
@@ -0,0 +1,30 @@
+import logging
+import shutil
+from . import util
+
+
+class Folder:
+
+    def __init__(self) -> None:
+        self.logger = logging.getLogger(__name__)
+
+    def copy(self, source_path: str, dest_path: str, ignore_pattern: list = None) -> str:
+
+        if not util.is_folder(source_path):
+            raise Exception(f"{source_path} is not a folder/dir")
+
+        if len(ignore_pattern) <= 0:
+            ignore_pattern = None
+
+        if ignore_pattern is not None:
+            self.logger.debug(f"ignoring by pattern: {ignore_pattern}")
+
+        try:
+            dest = shutil.copytree(src=source_path, dst=dest_path, dirs_exist_ok=True,
+                                   ignore=shutil.ignore_patterns(*ignore_pattern))
+            self.logger.debug("folder copied successfuly")
+
+            return dest
+        except Exception as err:
+            self.logger.exception(f"unable to copy folder from {source_path} to {dest_path}")
+            raise err
