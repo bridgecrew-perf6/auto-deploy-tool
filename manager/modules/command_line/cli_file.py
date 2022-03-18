15e8c94eeb6da22664355599bbf545d7549c152a
Guilherme Rodrigues <guilherme.b.rodrigues@skf.com>
2022-03-18 16:06:46 -0300

    test commit

--- manager/modules/command_line/cli_file.py
+++ manager/modules/command_line/cli_file.py
@@ -13,8 +13,8 @@ logger = logging.getLogger()
 def copy_file(source_path: str, dest_path: str) -> None:
     """Copy file from one path to other path"""
     try:
-        file_instance = file.File()
-        file_instance.copy_single(source_path, dest_path)
+        f = file.File()
+        f.copy_single(source_path, dest_path)
     except Exception as err:
         logger.exception(f"error to copy single file. err: {err}")
         typer.secho(f"[X] error to copy file from {source_path} to {dest_path}", fg="red")
